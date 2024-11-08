69a9d8ba2fda245ad8cce19bad843ea66b0c7e88
Dennis Bozzi <dennisbozzii@gmail.com>
2024-11-08 08:44:38 -0300

    Teste img

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -2,7 +2,7 @@ import logo from "@/assets/altimec5.png"
 
 export default function HomePage() {
   return (
-    <div className="bg-logo3 bg-fixed bg-cover bg-esmaecer bg-blend-lighten bg-center h-[600px] w-full flex flex-col justify-start items-center gap-20">
+    <div className="bg-logo3 bg-fixed bg-cover bg-esmaecer bg-blend-lighten bg-no-repeat bg-center h-[600px] w-full flex flex-col justify-start items-center gap-20">
       <img src={logo} alt="Logo altimec" width="240px" className="mt-24" />
       <div>
         <h1 className="text-4xl text-center px-4">"Mapeando o futuro com precisão e confiança."</h1>
